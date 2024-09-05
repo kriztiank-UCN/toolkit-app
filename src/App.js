77bb380b9382f68a0f49eb597cea78fc7ae25f3b
Kristian <10221425@ucn.dk>
2024-09-05 06:41:50 +0200

    Create the Jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useSelector, useDispatch } from "react-redux"
+// Import the increment and decrement actions
+import { increment, decrement } from "./counterState"
 import "./App.css"
 
+// Use state from the store
 function App() {
-  return <div className="App">Hello React </div>
+  // Get the counter state value from the store
+  const count = useSelector(state => state.counter.value)
+  // Get the dispatch function
+  const dispatch = useDispatch()
+  return (
+    <div className="App">
+      <div>Count: {count} </div>
+      {/* Dispatch the increment action from counterstate.js */}
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      {/* Dispatch the decrement action from counterstate.js */}
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+    </div>
+  )
 }
 
 export default App
--- src/counterState.js
+++ src/counterState.js
@@ -1,22 +1,22 @@
-import {createSlice} from 'from @redux/toolkit'
+import { createSlice } from "@reduxjs/toolkit"
 
 export const counterSlice = createSlice({
-  name: 'counter',
+  name: "counter",
   // Get
   initialState: {
-    value: 0
+    value: 0,
   },
   // Set
   reducers: {
-    increment: state => {
+    increment: (state) => {
       state.value += 1
     },
-    decrement: state => {
+    decrement: (state) => {
       state.value -= 1
-    }
-  }
+    },
+  },
 })
 
-export const {increment, decrement} = counterSlice.actions
+export const { increment, decrement } = counterSlice.actions
 
 export default counterSlice.reducer
