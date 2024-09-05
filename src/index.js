88f8eeb8e3a4768fab3b5c42baeff93f8276d07a
Kristian <10221425@ucn.dk>
2024-09-05 05:53:17 +0200

    setup toolkit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App"
+import { Provider } from "react-redux"
+import { configureStore } from "@reduxjs/toolkit"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+export const store = configureStore({
+  reducer: {},
+})
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-);
+  </Provider>
+)
